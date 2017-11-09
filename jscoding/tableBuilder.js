6aa63beedc398b0cafc0e20561307dc3607bdaef
Sehgal <ASEH@EUR.AD.SAG>
2017-11-09 21:12:27 +0530

    changes in jscoding

--- jscoding/tableBuilder.js
+++ jscoding/tableBuilder.js
@@ -2,6 +2,56 @@
 
 (()=>{
 
+   /************Table Elements Definitions****************/ 
+    //cell constructor for each cell in a row
+    function Cell(height,width,content){
+        this.height = height || 0;
+        this.width = width || 0;
+        this.content = content || "";
+
+    }
+    
+    //cell row  constructor
+    function CellRow(rowArray){
+        this.row = rowArray;
+    }
+    //adding getters and setters
+    Object.defineProperties(CellRow.prototype,"minHeight",{
+
+        get:function(){
+            return this.row.reduce(function(min,cell){
+                return Math.min(min,cell.height);
+            },Number.MAX_SAFE_INTEGER);
+        },
+        set: function(){
+            console.log("Cannot set the minimum height of a cell row");
+        }
+    });
+    Object.defineProperties(CellRow.prototype,"minWidth",{
+        
+        get:function(){
+            return this.row.reduce(function(min,cell){
+                return Math.min(min,cell.width);
+            },Number.MAX_SAFE_INTEGER);
+        },
+        set: function(){
+            console.log("Cannot set the minimum width of a cell row");
+        }
+    });
+
+
+    /**********End Table Elements Definitions***********/
+
+    /*************Utilities for table building***************/
+
+    //draw each cell row
+    function draw(width,height){
+        
+
+        var cellRow = new CellRow();
+
+    }
+
     //this function will return the maximum height of each cell in each row
     function rowHeights(rows){
 
@@ -21,6 +71,13 @@
         });
     }
 
-    //this function takes rows as the parameter and draws the table
+    //this function takes rows as the parameter and draws the table 
+    function drawTable(rows){
+        var rows = rowHeights(rows);
+        var cols = rowHeights(rows);
 
+        function drawLines(){
+            rows.
+        }
+    }
 })();
---
+++ jscoding/vector.js
@@ -0,0 +1,32 @@
+// playing around with vectors
+function Vector(x,y){
+    this.x = x;
+    this.y = y;
+    this.plus = function(vector){
+        if(vector instanceof Vector){
+            return new Vector(this.x+vector.x,this.y+vector.y);
+        }else{
+            console.log("Please pass a vector object");
+        }
+    }
+    this.minus = function(){
+        if(vector instanceof Vector){
+            return new Vector(this.x-vector.x,this.y-vector.y);
+        }else{
+            console.log("Please pass a vector object");
+        }
+        
+    }
+}
+
+Object.defineProperty(Vector.prototype,"length",{
+
+    get:function(){
+        return Math.sqrt(this.x*this.x + this.y*this.y);
+    }
+});
+
+var vector = new Vector(10,20);
+console.log(vector.length);
+var a = vector.plus(new Vector(10,10));
+console.log(a);
